be8307ed0556973836e8daf36b7a04fa7a6c975d
loliglitch <loliglitch@fedora.email>
2025-05-02 18:33:42 -0300

    colors update

--- lua/lualine/themes/selenized.lua
+++ lua/lualine/themes/selenized.lua
@@ -1,22 +1,5 @@
 local colors = selenized.colors
 return {
-  bg_0    =  '#ffffff',
-  bg_1    =  '#ebebeb',
-  bg_2    =  '#cdcdcd',
-  dim_0   =  '#878787',
-  fg_0    =  '#474747',
-  fg_1    =  '#282828',
-  yellow  =  '#c49700',
-  orange  =  '#d04a00',
-  red     =  '#d6000c',
-  magenta =  '#dd0f9d',
-  violet  =  '#7f51d6',
-  blue    =  '#0064e4',
-  cyan    =  '#00ad9c',
-  green   =  '#1d9700',
-}
-
-return {
   normal = {
     a = { fg = colors.bg_0, bg = colors.blue, gui = 'bold' },
     b = { fg = colors.fg_1, bg = colors.bg_2 },
