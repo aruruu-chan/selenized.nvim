89b5a36dac1a62576318399a1b159b00416bb788
loliglitch <loliglitch@fedora.email>
2025-09-06 14:09:27 -0300

    bold

--- lua/lualine/themes/selenized.lua
+++ lua/lualine/themes/selenized.lua
@@ -1,13 +1,13 @@
 local colors = selenized.colors
 return {
   normal = {
-    a = { fg = colors.bg_0, bg = colors.blue, gui = 'bold' },
+    a = { fg = colors.bg_0, bg = colors.br_blue, gui = 'bold' },
     b = { fg = colors.fg_1, bg = colors.bg_2 },
     c = { fg = colors.fg_1, bg = colors.bg_1 },
   },
-  insert = { a = { fg = colors.bg_0, bg = colors.green, gui = 'bold' } },
-  visual = { a = { fg = colors.bg_0, bg = colors.magenta, gui = 'bold' } },
-  replace = { a = { fg = colors.bg_0, bg = colors.red, gui = 'bold' } },
+  insert = { a = { fg = colors.bg_0, bg = colors.br_green, gui = 'bold' } },
+  visual = { a = { fg = colors.bg_0, bg = colors.br_magenta, gui = 'bold' } },
+  replace = { a = { fg = colors.bg_0, bg = colors.br_red, gui = 'bold' } },
   inactive = {
     a = { fg = colors.dim_0, bg = colors.bg_2, gui = 'bold' },
     b = { fg = colors.dim_0, bg = colors.bg_2 },
